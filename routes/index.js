c0f76352f20976e553121a4e37654f09f61a5aec
Justin <justinrkinsman@gmail.com>
2022-12-08 10:29:32 -0400

    Create login and signup page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.pug', { title: 'DIE!!!' });
+  res.render('index.pug', { title: 'Home Page' });
 });
+
+/* GET sign up page */
+router.get('/sign-up', function(req, res, next) {
+  res.render('sign-up.pug', {title: 'Sign Up'})
+})
+
+/* GET login page */
+router.get('/login', function(req, res, next) {
+  res.render('login.pug', {title: "Log In"})
+})
 
 module.exports = router;
