3686c7919c515ce777cf405f0ac3db48ed169368
Justin <justinrkinsman@gmail.com>
2022-12-08 10:15:47 -0400

    Change jade to pug

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(function (err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500)
-    res.render('error.jade')
+    res.render('error.pug')
 })
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.jade', { title: 'DIE!!!' });
+  res.render('index.pug', { title: 'DIE!!!' });
 });
 
 module.exports = router;
