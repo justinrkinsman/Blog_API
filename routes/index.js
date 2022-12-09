53f02d2cfb2499025cb31247a81846834ac92db2
Justin <justinrkinsman@gmail.com>
2022-12-09 08:13:31 -0400

    Add basic routes for main pages

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+/// GET Routes ///
+
+/* GET home page */
 router.get('/', (req, res) => {
-    return res.send("Received a GET HTTP method")
+    return res.render("index.pug", { title: "Home Page" })
+})
+
+/* GET login page */
+router.get('/login', (req, res) => {
+    return res.render("login.pug", { title: "Log In" })
+})
+
+/* GET signup page */
+router.get('/sign-up', (req, res) => {
+    return res.render('sign-up.pug', { title: "Sign Up" })
+})
+
+/* GET failed login page */
+/// CHANGE THIS SO USERS CAN'T ACCESS IT MANUALLY ///
+router.get('/failed-login', (req, res) => {
+    return res.render('failed-login.pug', { title: "Log-in Attempt Failed" })
+})
+
+/* GET successful login page */
+/// REMOVE THIS LATER ///
+router.get('/success-login', (req, res) => {
+    return res.render('success-login.pug')
 })
 
 router.post('/', (req, res) => {
